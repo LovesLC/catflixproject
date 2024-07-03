9189f294bf460c4b159105254ab7dfbdc842dd9b
João Victor da Silva <104177356+LovesLC@users.noreply.github.com>
2024-07-03 14:48:09 -0300

    Add files via upload

---
+++ frontend/assets/js/header.js
@@ -0,0 +1,24 @@
+//Código pro menu dropdown!
+var dropdownBtn = document.getElementById('dropdownBtn'); //Armazenando o botão do menu na variável.
+var dropdownEl = document.querySelector('.dropdown'); //Armazenando o menu dropdown na variável.
+const menu = document.getElementById('dropdownMenu')
+
+//Adicionando um evento de click no botão do menu dropdown pra trocar o display da div com os botões.
+dropdownBtn.addEventListener('click', function() {
+
+    if (dropdownEl.style.display === 'block'){
+    dropdownEl.style.display = 'none'
+    } else {
+        dropdownEl.style.display = 'block'
+    }
+});
+
+//Adicionando um evento pra caso o menu dropdown estiver em uma situação diferente da função acima, verificar e mudar para display none.
+document.addEventListener('click', function(event) {
+    if (event.target !== dropdownBtn) {
+        dropdownEl.style.display = "none";
+    }
+});
+
+
+
